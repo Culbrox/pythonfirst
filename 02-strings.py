6fc03ddc85f74402190fe87367b9844281fcc6cc
Francisco Garcia Valles <culbrox125@gmail.com>
2023-10-01 17:22:08 -0300

    Create 02-strings.py

---
+++ 02-strings.py
@@ -0,0 +1,11 @@
+texto = "Hola Mundo"
+
+print (texto.upper())
+print (texto.lower())
+print (texto.find("Mun"))
+print (texto.replace("Mun", "chanchito feliz"))
+nuevoTexto = texto.replace("Mun", "chanchito feliz")
+print(texto, nuevoTexto)
+print("Mundo" in texto)
+
+
