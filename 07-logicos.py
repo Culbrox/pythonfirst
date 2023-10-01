3e2b6a6cddf8b327b3e07dc08b8b1b86b9caf2da
Francisco Garcia Valles <culbrox125@gmail.com>
2023-10-01 18:45:25 -0300

    modified logicos,py

--- 07-logicos.py
+++ 07-logicos.py
@@ -1,3 +1,5 @@
 edad = 22 
 
 print(edad > 18 and edad < 30) 
+print(edad > 18 or edad < 30) 
+print(not (edad > 17))  
---
+++ 08-if.py
@@ -0,0 +1,10 @@
+puntaje = 95
+
+if puntaje >= 95:
+    print("Aprobado con honores")
+elif puntaje >= 70:
+        print("Aprobado")
+else:
+    print("Reprobado")
+
+print("Fuera del if") 
