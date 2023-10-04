8aea84b8b32c2c85a97a5b56086d882cd7d76c0e
Francisco Garcia Valles <culbrox125@gmail.com>
2023-10-04 00:07:35 -0300

    modified

---
+++ prueba.py
@@ -0,0 +1,15 @@
+temperatura = float(input("Ingrese temperatura:"))
+
+escala = input("es Fahrenheit(F) o celcius(C)?:").lower()
+
+while True:
+    def celcius_a_fahrenheit(escala, temperatura):
+        if escala == "f":
+            celsius = (temperatura - 32) * 5/9
+    def fahrenheit_a_celcius(escala, temperatura):
+        if escala == "c":
+            fahrenheit = temperatura * 1.8 + 32
+    if escala == NameError():
+        print("escala incorrecta")
+    else:
+        break
