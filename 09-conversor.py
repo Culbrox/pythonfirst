5d274324e56f5a93f215c4ae11bf22da6f4009c2
Francisco Garcia Valles <culbrox125@gmail.com>
2023-10-04 00:07:43 -0300

    modified

--- 09-conversor.py
+++ 09-conversor.py
@@ -8,10 +8,26 @@ if escala == "f":
 elif escala == "c":
     fahrenheit = temperatura * 1.8 + 32
     print(fahrenheit)
-else:
+else :
     print("escala incorrecta")
 
 
+
+
+
+#def celcius_a_fahrenheit():
+#    if escala == "f":
+#        celsius = (temperatura - 32) * 5/9
+#def fahrenheit_a_celcius():
+#    if escala == "c":
+#        fahrenheit = temperatura * 1.8 + 32
+
+#            if escala == "f":
+#   celsius = (temperatura - 32) * 5/9
+#  print(celsius)
+# if escala == "c":
+#        fahrenheit = temperatura * 1.8 + 32
+#    print(fahrenheit)
 
         #if = si
         #elif = else if = sino
